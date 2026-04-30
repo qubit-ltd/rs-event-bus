5d892e68d7678735add3f5ea4955d2dedd2852a5
Haixing Hu <starfish.hu@gmail.com>
2026-04-30 20:18:05 +0800

    chore: scaffold event bus crate

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2026.
+ *    Haixing Hu, Qubit Co. Ltd.
+ *
+ *    All rights reserved.
+ *
+ ******************************************************************************/
+//! # Qubit Event Bus
+//!
+//! A lightweight, thread-safe in-process event bus for Rust.
+//!
+//! # Author
+//!
+//! Haixing Hu
+
+#![deny(missing_docs)]
