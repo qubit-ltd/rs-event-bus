36daf9144635c80b0111d5e487f96637d81391a4
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 12:29:39 +0800

    docs: standardize source file attribution

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
 /*******************************************************************************
  *
- *    Copyright (c) 2026.
- *    Haixing Hu, Qubit Co. Ltd.
+ *    Copyright (c) 2026 Haixing Hu.
  *
- *    All rights reserved.
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
  *
  ******************************************************************************/
 //! # Qubit Event Bus
 //!
 //! A lightweight, thread-safe in-process event bus for Rust.
 //!
-//! # Author
-//!
-//! Haixing Hu
 
 #![deny(missing_docs)]
 
